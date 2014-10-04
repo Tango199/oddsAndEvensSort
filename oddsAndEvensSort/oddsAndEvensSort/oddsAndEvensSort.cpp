d87484af9ed3ca5c46f9c777bdbe9b2adb3c2d53
rxs5346 <rxs5346@sru.edu>
2014-10-04 13:44:43 -0400

    Serial done

--- oddsAndEvensSort/oddsAndEvensSort/oddsAndEvensSort.cpp
+++ oddsAndEvensSort/oddsAndEvensSort/oddsAndEvensSort.cpp
@@ -1,34 +1,56 @@
 #include <iostream>
 #include <time.h>
 using namespace std;
-const int n = 1000;
-int a[n] = {};
-int phase;
-int i;
 void getRandom();
 void doSort();
 void swap();
+void doPrint();
+
+
+const int n = 100000;
+int a[n] = {};
+int phase;
+int i;
 
 int main()
 {
+	clock_t start,end;
+	srand(time(0));
+
+	float clockDiff =0.0;
 	getRandom();
+	start = clock();
+	//doPrint();
 	doSort();
-	
+	//doPrint();
+	end = clock();
+	clockDiff = (float)((end - start)/CLOCKS_PER_SEC);
+	cout << "Array is Sorted, it took " << clockDiff <<" seconds" << endl;
+	system("pause");
 	return 0;
+}
+void doPrint()
+{
+	cout << endl;
+	for(int i=0; i < n; i++)
+	{
+		cout << a[i] << endl;
+	}
 }
 void getRandom()
 {
-	srand (time(NULL));
+	
 	for(int i=0; i < n; i++)
 	{
 		a[i] = rand() % 1000 + 1;
-		cout << a[i] << endl;
+		//cout << a[i] << endl;
 	}
 }
 void doSort()
 {
 	for(phase =0; phase < n; phase++)
 	{
+	
 		if(phase % 2 == 0)
 		{
 			for(i=1;i<n;i+=2)
@@ -36,6 +58,7 @@ void doSort()
 				if(a[i-1] > a[i])
 				{
 					swap(a[i-1],a[i]);
+					//cout << "swapped" << endl;
 				}
 			}
 		}
@@ -46,17 +69,19 @@ void doSort()
 				if(a[i] > a[i+1])
 				{
 					swap(a[i],a[i+1]);
+					//cout << "swapped" << endl;
 				}
 			}
 		}
+		
 	}
 
 }
-void swap(int swapFrom[],int swapTo[])
-{
-	//this function isnt correct
-	int temp=swapFrom[i];
-	swapFrom[i]=swapTo[i];
-	swapTo[i]=temp;
-
-}
+//void swap(int swapFrom[],int swapTo[])
+//{
+//	//this function isnt correct
+//	int temp=swapFrom[i];
+//	swapFrom[i]=swapTo[i];
+//	swapTo[i]=temp;
+//
+//}
